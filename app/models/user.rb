1c8b02156b99dfe26cdfc416abf5bcc56b5a4204
齋藤陸弥 <rikuya6@gmail.com>
2016-08-28 22:12:33 +0900

    第6章終了 第7章からスタート

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   validates :name,      presence: true,
                         length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,     presence: true,
                         length: { maximum: 255 },
                         uniqueness: { case_sensitive: false },
