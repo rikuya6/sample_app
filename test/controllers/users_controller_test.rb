3afdfc30e6bf8fd98ed1e56296eabedb6e9d8195
齋藤陸弥 <rikuya6@gmail.com>
2016-09-04 22:13:35 +0900

    Chapter9終了

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,7 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
@@ -35,6 +35,26 @@ class UsersControllerTest < ActionController::TestCase
     log_in_as(@other_user)
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect index when not logged in' do
+    get :index
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
     assert_redirected_to root_url
   end
 end
